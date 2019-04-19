66c000a438f69df9a434ce717297bbf963571a47
Unknown <brandonbixler21@yahoo.com>
2019-04-19 12:53:58 -0400

    Search page done

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../models/item.dart';
+
+import '../widgets/search_tile.dart';
+
+import '../globals.dart' as globals;
+
 class SearchPage extends StatefulWidget{
 
   @override
@@ -7,11 +13,87 @@ class SearchPage extends StatefulWidget{
 }
 
 class SearchPageState extends State<SearchPage>{
+  final _formKey = new GlobalKey<FormState>();
+
+  List<Item> searchList = <Item>[];
+  String _search;
 
   @override
   Widget build(BuildContext context){
     return new Scaffold(
-
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Search"),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  _showSearchBar(),
+                  _showSearchbutton()
+                ],
+              ),
+              Expanded(
+                flex: 17,
+                child: (searchList != null && searchList.isNotEmpty) ? ListView.builder(
+                  itemCount: searchList.length,
+                  itemBuilder: (context, index) => SearchTile(searchList[index]),
+                ) : Container(),
+              )
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Widget _showSearchBar(){
+    return Expanded(
+      flex: 4,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10.0, 5.0, 5.0, 0.0),
+        child: TextFormField(
+          maxLines: 1,
+          autofocus: false,
+          decoration: new InputDecoration(
+            hintText: 'Search',
+          ),
+          onSaved: (value) => _search = value,
+        ),
+      ),
+    );
+  }
+
+  Widget _showSearchbutton(){
+    return Expanded(
+      flex: 1,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(5.0, 5.0, 0.0, 0.0),
+        child: new IconButton(
+          icon: Icon(Icons.search),
+          color: Color.fromARGB(255, 6, 98, 157),
+          onPressed: _searchForItem,
+        )
+      ),
+    );
+  }
+
+  void _searchForItem(){
+    _formKey.currentState.save();
+    List<Item> tempList = <Item>[];
+    for(final i in globals.items){
+      if(i.name.toLowerCase().contains(_search.toLowerCase(), 0)){
+        tempList.add(i);
+      }
+    }
+    setState(() {
+      searchList = tempList;
+    });
   }
 }
---
+++ lib/widgets/search_tile.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import '../models/item.dart';
+import '../models/itemTypes.dart';
+
+class SearchTile extends StatelessWidget{
+  final dynamic _item;
+  SearchTile(this._item);
+
+  @override
+  Widget build(BuildContext context){
+    return InkWell(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(_item.name),
+            subtitle: Text(CategoryEnumMap[_item.category]),
+            leading: Container(
+              margin: EdgeInsets.only(left: 6.0),
+              child: (_item.picPath != null && _item.picPath != "") ? (_item is Game && _item.isSteamGame) ? Image.network(_item.picPath, height: 50.0,) : Image.memory(base64.decode(_item.picPath), height: 50.0, fit: BoxFit.fill,): Center(child: Text("No Image")),
+            ),
+          ),
+          Divider()
+        ],
+      ),
+      onTap: (){},
+    );
+  }
+}
